03a75ef50547249b66f0cc7bfbbbaea8a6008caf
SuhasC572 <84210024+SuhasC572@users.noreply.github.com>
2021-05-19 19:51:46 +0530

    First commit

---
+++ soc.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myPassword");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function Function2() {
+    alert("You have Logged in Successfully :)");
+}
